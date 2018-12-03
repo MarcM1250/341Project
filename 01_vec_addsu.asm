95858a2054c86669fbf48fadf8697d741d154b04
MarcM1250 <36463798+MarcM1250@users.noreply.github.com>
2018-12-02 18:03:43 -0800

    Update 01_vec_addsu.asm

--- 01_vec_addsu.asm
+++ 01_vec_addsu.asm
@@ -8,8 +8,8 @@
 # Description:      This baseline instruction performs a vector add saturated (unsigned)
 #		    operation. Each element of a is added to the corresponding element of b.
 #		   
-# Register Usage:   $s6: Upper 32 bits of vector d
-#		    $s7: Lower 32 bits of vector d 
+# Register Usage:   $s0: Upper 32 bits of vector d
+#		    $s1: Lower 32 bits of vector d 
 #		    $t0: Upper 32 bits of vector a
 #		    $t1: Lower 32 bits of vector a
 #		    $t2: Upper 32 bits of vector b
@@ -39,12 +39,12 @@
 main:      # ************* Code starts here ***********
 
            # Setting testing values
-           addi $t0, $zero, 0x233C475D 	        # Lower 32 bits of vector a
-           addi $t1, $zero, 0x087F196F		# Upper 32 bits of vector a
-           addi $t2, $zero, 0x981963C5 		# Lower 32 bits of vector b       
-           addi $t3, $zero, 0x5E80B36E		# Upwer 32 bits of vector b
-           addi $s0, $zero, 0			# Lower 32 bits of d = 0
-           addi $s1, $zero, 0			# Upper 32 bits of d = 0
+           addi $t0, $zero, 0x233C475D 	        # Upper 32 bits of vector a
+           addi $t1, $zero, 0x087F196F		# Lower 32 bits of vector a
+           addi $t2, $zero, 0x981963C5 		# Upper 32 bits of vector b       
+           addi $t3, $zero, 0x5E80B36E		# Lower 32 bits of vector b
+           addi $s0, $zero, 0			# Upper 32 bits of d = 0
+           addi $s1, $zero, 0			# Lower 32 bits of d = 0
            
            addi $v1, $zero, 0			# Setting register to count bytes (8 bytes) d[i]
            addi $t8, $0, 0xFF000000		# Mask to get 8 bits a[i], b[i]
