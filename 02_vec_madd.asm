9deac622926c53e915db4ffbd1b516f350b9caa4
MarcM1250 <36463798+MarcM1250@users.noreply.github.com>
2018-12-02 19:42:21 -0800

    Update 02_vec_madd.asm

--- 02_vec_madd.asm
+++ 02_vec_madd.asm
@@ -1,6 +1,6 @@
 #*************************** 3 4 1   T o p    L e v e l   M o d u l e *************************
 #
-# File name:        01_vec_addsu.asm
+# File name:        02_vec_madd.asm
 # Version:          1.0
 # Date:             December 3, 2018  
 # Programmers:      Anton Carrillo && Marc Marcelino
